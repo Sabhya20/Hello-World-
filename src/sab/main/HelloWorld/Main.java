cf635f0243f445e03e3549cd221d8b0e3da3708e
DayDev <sabhyasoni20@gmail.com>
2022-05-25 00:01:48 +0530

    Mistake

--- src/sab/main/HelloWorld/Main.java
+++ src/sab/main/HelloWorld/Main.java
@@ -3,7 +3,8 @@ package sab.main.HelloWorld;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+		System.out.println("Hello World");
 		
 
 	}
